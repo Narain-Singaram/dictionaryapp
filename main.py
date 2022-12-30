f28fb2bd680938617858afd2fd482984cca42b8c
Narain Singaram <narain.awesome@gmail.com>
2022-12-29 23:47:15 -0500

    fixed the error change python file directory that was the problem

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
+
 @app.route('/')
-def index_pg():
-    return render_template("index.html")
+def home():
+    return render_template('index.html')
+
+
 @app.route("/api/v1/<word>")
 def api_pg(word):
     definition = word.upper()
     result_dictionary = {"word": word, 'definition': definition}
     return result_dictionary
 
-if(__name__ == "__main__"):
-    app.run(debug=True,port=5001)
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5001)
