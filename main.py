edbb47e2218c0315e0cde0e6ba59b6ba86302b20
Narain Singaram <narain.awesome@gmail.com>
2022-12-29 23:10:09 -0500

    idk

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 from flask import Flask, render_template
+
+app = Flask(__name__)
+@app.route('/')
+def index_pg():
+    return render_template("index.html")
+@app.route("/api/v1/<word>")
+def api_pg(word):
+    definition = word.upper()
+    result_dictionaru
