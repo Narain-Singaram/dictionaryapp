8ed9a8c2573f86a8472faadd6904d385b728d88a
Narain Singaram <narain.awesome@gmail.com>
2022-12-30 18:12:33 -0500

    word table and main.py changes

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 from flask import Flask, render_template
+import pandas as pd
 
 app = Flask(__name__)
+
+df = pd.read_csv("word_table.csv")
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
-
 @app.route("/api/v1/<word>")
 def api_pg(word):
-    definition = word.upper()
+    definition = df.loc[df["word"] == word]['definition'].squeeze()
     result_dictionary = {"word": word, 'definition': definition}
     return result_dictionary
 
